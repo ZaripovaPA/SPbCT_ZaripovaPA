4edb0200cb20de890d7e8c92dbee7c759d86de74
ZaripovaPolina891 <77537150+ZaripovaPolina891@users.noreply.github.com>
2021-01-16 17:12:13 +0300

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Java {
+
+
+        public static void main (String args []) {
+
+            double a = 3;
+
+            double b = 4;
+
+            double с;
+
+            с = Math.sqrt (a* a + b* b);
+
+            System.out.println ("c = "+ с);
+
+        }
+}
